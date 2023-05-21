7630d96d89dc5f6baecd57ce59b214b07083dcec
Jacinto Roldán Rodríguez <balbino.v12@gmail.com>
2023-05-21 15:32:36 +0200

    create proyect wiht spring initializr

---
+++ src/test/java/com/sdjr2/rest_contact_meanssb/MicroContactMeansApplicationTests.java
@@ -0,0 +1,15 @@
+package com.sdjr2.rest_contact_meanssb;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroContactMeansApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertTrue(Boolean.TRUE);
+	}
+
+}
