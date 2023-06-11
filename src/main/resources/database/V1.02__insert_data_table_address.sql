e4c34eca032eb405f08dc8bad76276ca24aa40d6
Jacinto Roldán Rodríguez <balbino.v12@gmail.com>
2023-06-11 15:05:01 +0200

    update .sql about mysql

---
+++ src/main/resources/database/V1.02__insert_data_table_address.sql
@@ -0,0 +1,8 @@
+--
+-- Dumping data for table `address`
+--
+INSERT INTO `db_ca_viso`.`address` VALUES (address_id, street, number, letter, town, city, country, postal_code, latitude, longitude, additional_info)
+	(1,'Avda Piedra el Gallo',22,NULL,'El Viso del Alcor','Sevilla','España',41520,37.391060,-5.7199300,'Pabellón de deportes Santa Lucía, Planta 1')
+	(2,'Corredera',155,'A','El Viso del Alcor','Sevilla','España',41520,-5.7199300,37.391060,'Vivo actualmente en el Piso'),
+	(3,'Velázquez',17,NULL,'El Viso del Alcor','Sevilla','España',41520,-5.7199300,37.391060,'Bajo'),
+	(4,'Avda Republica Nicaragua',1,NULL,'El Viso del Alcor','Sevilla','España',41520,37.391060,-5.7199300,'Piso');
